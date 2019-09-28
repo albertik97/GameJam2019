5cdfbb171c50ea9cb7a9bf9e5b8569c97043b2a5
Angel Terol <egenad8@gmail.com>
2019-09-28 11:34:50 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ gamejam/Assets/Scripts/timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public Text counter;
+    public float seconds, minutes, last_seconds;
+
+    // Start is called before the first frame update
+    void Start(/*float mins*/)
+    {
+        seconds = 0.0f;
+        minutes = 3f;
+        counter = GetComponent<Text>() as Text;
+        counter.text = "03:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(seconds < 0){
+            seconds = 59;
+            minutes--;
+        }
+        else{
+
+            float actual_seconds = (int)(Time.time % 60f);
+
+            if (actual_seconds - last_seconds != 0)
+            {
+                seconds--;
+                last_seconds = (int)(Time.time % 60f);
+            }
+        }
+        counter.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+    }
+}
