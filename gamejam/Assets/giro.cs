497c6b0fb89a7178697e5ad0b13de9ae6d08e80f
Alejandro Domenech <a.domenechvicedo@gmail.com>
2019-09-28 12:38:34 +0200

    mergee

---
+++ gamejam/Assets/giro.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class giro : MonoBehaviour
+{
+    private bool gira;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gira = false;
+    }
+
+// Update is called once per frame
+void Update()
+{
+        Debug.Log(transform.rotation.z);
+        
+    if (transform.rotation.z > 0.1f)
+        gira = true;
+
+    if (transform.rotation.z < -0.1f)
+       gira = false;
+
+     if(gira)
+        transform.Rotate(new Vector3(0, 0, -50) * Time.deltaTime);
+     else 
+        transform.Rotate(new Vector3(0, 0, 50) * Time.deltaTime);
+}
+}
---
+++ gamejam/Assets/veneno.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class veneno : MonoBehaviour
+{
+    public GameObject venenos;
+    public GameObject queso;
+    private float step = 3.8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int Npotions = Random.Range(1, 3);
+        Instantiate(queso, new Vector3(0, 0.1f, 0), Quaternion.identity);
+        for (int i = 0; i < Npotions; i++)
+        {
+            int ran = Random.Range(-5, 6);
+            int ran2 = Random.Range(-5, 6);
+            Instantiate(venenos, new Vector3(step * ran, step*ran2, 0), Quaternion.identity);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
