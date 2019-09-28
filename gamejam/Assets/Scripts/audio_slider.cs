6e2dcb5eb6ef66162e1d4060d580c23f18043b36
Angel Terol <egenad8@gmail.com>
2019-09-28 12:22:48 +0200

    audio arreglado

---
+++ gamejam/Assets/Scripts/audio_slider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class audio_slider : MonoBehaviour
+{
+    private AudioSource audio;
+
+    private float volume = 1f;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        audio.volume = volume; 
+    }
+
+    public void SetVolume(Slider slider)
+    {
+        volume = slider.value;
+    }
+}
