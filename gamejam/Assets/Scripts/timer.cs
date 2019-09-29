3c26525e0ff0790569a5c688e078deeacd4dc77d
Angel Terol <egenad8@gmail.com>
2019-09-29 11:23:23 +0200

    Auto stash before merge of "master" and "origin/master"

--- gamejam/Assets/Scripts/timer.cs
+++ gamejam/Assets/Scripts/timer.cs
@@ -6,7 +6,8 @@ using UnityEngine.UI;
 public class timer : MonoBehaviour
 {
     public Text counter;
-    public float seconds, minutes, last_seconds, changer;
+    public float seconds, minutes, last_seconds, changer, swtch;
+    bool active;
     public GameObject goc;
     // Start is called before the first frame update
     void Start(/*float mins*/)
@@ -15,6 +16,8 @@ public class timer : MonoBehaviour
         last_seconds = 0;
         minutes = 0f;
         changer = 0f;
+        active = false;
+        swtch = 0f;
         counter = GetComponent<Text>() as Text;
         counter.text = "03:00";
     }
@@ -22,6 +25,21 @@ public class timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (active)
+        {
+            if(swtch >= 1)
+            {
+                active = false;
+                swtch = 0f;
+
+                GameObject a = GameObject.FindGameObjectWithTag("gamecanvas");
+                GameObject ch0 = a.transform.GetChild(7).gameObject;
+                ch0.SetActive(false);
+                GameObject ch1 = a.transform.GetChild(8).gameObject;
+                ch1.SetActive(false);
+            }
+        }
 
         if(seconds < 0 && minutes > 0){
             seconds = 59;
@@ -36,6 +54,10 @@ public class timer : MonoBehaviour
                 seconds--;
                 last_seconds = (int)(Time.time % 60f);
                 changer++;
+                if (active)
+                {
+                    swtch++;
+                }
             }
         }
         counter.text = minutes.ToString("00") + ":" + seconds.ToString("00");
@@ -52,6 +74,18 @@ public class timer : MonoBehaviour
             changer = 0;
             goc.swap();
             c.setControls();
+
+            //Show switch images
+            if (active == false) {
+                active = true;
+                swtch = 0f;
+
+                GameObject a = GameObject.FindGameObjectWithTag("gamecanvas");
+                GameObject ch0 = a.transform.GetChild(7).gameObject;
+                ch0.SetActive(true);
+                GameObject ch1 = a.transform.GetChild(8).gameObject;
+                ch1.SetActive(true);
+            }
         }
     }
 
